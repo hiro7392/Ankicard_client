2fe4531a4c4c381d020590560e01501e1b42d165
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-07-29 21:43:48 +0900

    ホーム画面を一部修正

--- src/atoms/AppTitle.tsx
+++ src/atoms/AppTitle.tsx
@@ -4,8 +4,8 @@ export const AppTitle=()=>{
     return(
         <>
             <h1 className="font-mono text-8xl text-slate-700 mt-20">
-                分散学習<br/>
-                Card
+            Socrates<br/>
+            Maker
             </h1>
             <BookOpenIcon className="h-1/4 w-1/4 m-auto text-white"/>
         </>
