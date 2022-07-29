613877d1955db53dd526d6151bf54e0db3391dbf
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-07-29 21:45:55 +0900

    ホーム画面を一部修正

--- src/atoms/AppTitle.tsx
+++ src/atoms/AppTitle.tsx
@@ -5,7 +5,7 @@ export const AppTitle=()=>{
         <>
             <h1 className="font-mono text-8xl text-slate-700 mt-20">
             Socrates<br/>
-            Maker
+            Card
             </h1>
             <BookOpenIcon className="h-1/4 w-1/4 m-auto text-white"/>
         </>
