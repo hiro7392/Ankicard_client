f465bd9a436a48870517f43c42e3dd472213e91b
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-09-23 01:31:00 +0900

    ホーム画面にボタンを追加

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom"
 import { AppTitle } from "../atoms/AppTitle"
 import {Header} from "../templates/Header"
 export const HomePage=()=>{
     return(
         <>
         <Header/>
-        <div className="flex bg-slate-100 shadow-md rounded-lg w-1/2  mx-auto px-8 pt-6 pb-24 mb-4 mt-24 flex-col font-mono">
+        <div className="flex bg-slate-100 shadow-md rounded-lg w-1/2  mx-auto px-8 pt-6 pb-24 mb-20 mt-24 flex-col font-mono">
             <AppTitle/>
         </div>
+    
+        <Link
+            className="text-xl my-8 py-4 px-4  bg-teal-500 hover:bg-teal-700 text-white font-bold rounded"
 
+            to="/cards"
+        >
+            学習を始める
+        </Link>
         </>
     )
 }
